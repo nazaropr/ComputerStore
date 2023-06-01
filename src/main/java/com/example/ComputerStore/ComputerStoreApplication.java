79689a74ee4ed9c8ed037e75e4c100ce7f3b0333
nazaropr <90557490+nazaropr@users.noreply.github.com>
2023-06-02 01:33:01 +0300

    add first test controller

--- src/main/java/com/example/ComputerStore/ComputerStoreApplication.java
+++ src/main/java/com/example/ComputerStore/ComputerStoreApplication.java
@@ -3,18 +3,12 @@ package com.example.ComputerStore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class ComputerStoreApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ComputerStoreApplication.class, args);
 	}
-	@GetMapping("/")
-	public String hello(){
-		return "HELLO WORLD!!";
-	}
-
 }
