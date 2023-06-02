26118757f5fe2e31f49c206340bcbb835d61f603
nazaropr <90557490+nazaropr@users.noreply.github.com>
2023-06-02 15:16:26 +0300

    test mapping

--- src/main/java/com/example/ComputerStore/Hellocontroller.java
+++ src/main/java/com/example/ComputerStore/Hellocontroller.java
@@ -1,14 +1,25 @@
 package com.example.ComputerStore;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class Hellocontroller {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String hello(){
-        return "HELLO WORLD!!";
+    @GetMapping("test")
+    public String index(Model model) {
+        // Додайте дані моделі, якщо вони необхідні
+        model.addAttribute("message", "Привіт, світ!");
+
+        return "index";
     }
+    @GetMapping("/api")
+    public String Api(){
+        return "this is api";
+    }
+
 }
